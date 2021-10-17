7923c0db4b573f65067ddb3a5df62af6b328459c
Mikhail <16112000m@gmail.com>
2021-10-17 13:28:58 +0300

    add: solutions of Dynamic Programming 2nd day

---
+++ 70 Climbing Stairs/sol.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun climbStairs(n: Int): Int {
+        if (n == 1)
+            return 1
+        val res: MutableList<Int> = arrayListOf(1, 1, 2)
+
+
+        for(i in 3..n){
+            res.add(res[i-1] + res[i-2])
+
+        }
+        return res[n]
+    }
+}
