c25212b31bdc2c9fea78376b60c6d0f9819e5254
Mikhail <16112000m@gmail.com>
2022-04-30 00:55:02 +0300

    rem: f

--- 746 Min Cost Climbing Stairs/sol.kt
+++
@@ -1,9 +0,0 @@
-class Solution {
-    fun minCostClimbingStairs(cost: IntArray): Int {
-        val size_ = cost.size
-        for (i in 2..size_-1) {
-            cost[i] = Math.min(cost[i] + cost[i-1], cost[i] + cost[i-2])
-        }
-        return Math.min(cost[size_-1], cost[size_-2])
-    }
-}
