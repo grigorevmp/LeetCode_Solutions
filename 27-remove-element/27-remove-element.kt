410a018c23b127d1390b8cbae4d8d75b04b461ad
Mikhail Grigorev <16112000m@gmail.com>
2022-05-02 18:10:24 +0300

    Time: 249 ms (48.12%), Space: 35.2 MB (68.94%) - LeetHub

---
+++ 27-remove-element/27-remove-element.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun removeElement(nums: IntArray, num: Int): Int {
+        var counter = 0
+        nums.forEach { if (it != num) nums[counter++] = it }
+        return counter
+    }
+}
