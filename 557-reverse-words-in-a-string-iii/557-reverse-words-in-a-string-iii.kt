c500b9d948e1267c528ca323ef25c52828415a48
Mikhail Grigorev <16112000m@gmail.com>
2022-08-20 00:22:22 +0300

    Time: 323 ms (85.22%), Space: 55.8 MB (62.61%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun reverseWords(s: String): String {
+        var index = 0
+        var start = 0
+        var s2 = s.toCharArray() + charArrayOf(' ')
+        while (index != s2.size-1){
+            if ((index == s2.size-1) or (s2[index+1] == ' ')){
+                var end = index
+                for (char in 0..(end-start)/2){
+                    val tmp = s2[end]
+                    s2[end--] = s2[start]
+                    s2[start++] = tmp
+                }
+                start = index
+            }
+            else if(s2[index] == ' ')
+                start = index + 1 
+            index++
+        }
+        return String(s2).dropLast(1)
+    }
+}
