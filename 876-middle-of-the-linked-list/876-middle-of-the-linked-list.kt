52db13086f0c2bcfd99e661ed25f0f2c9b7742a9
Mikhail Grigorev <16112000m@gmail.com>
2022-08-20 21:36:11 +0300

    Time: 231 ms (46.56%), Space: 34 MB (60.45%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.kt
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.kt
@@ -1,12 +1,3 @@
-/**
- * Example:
- * var li = ListNode(5)
- * var v = li.`val`
- * Definition for singly-linked list.
- * class ListNode(var `val`: Int) {
- *     var next: ListNode? = null
- * }
- */
 class Solution {
     fun middleNode(head: ListNode?): ListNode? {
         var pointer = 0
